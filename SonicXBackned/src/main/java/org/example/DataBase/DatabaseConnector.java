dbb109d20fb2322cbecdb754142b872f90a6045c
Deniz <134473815+DenizEkber@users.noreply.github.com>
2024-05-25 15:26:47 +0400

    Update DatabaseConnector.java

--- SonicXBackned/src/main/java/org/example/DataBase/DatabaseConnector.java
+++ SonicXBackned/src/main/java/org/example/DataBase/DatabaseConnector.java
@@ -6,7 +6,7 @@ import java.sql.SQLException;
 
 public class DatabaseConnector {
 
-    private static final String JDBC_URL = "jdbc:mysql://localhost:3306/musicdata";
+    private static final String JDBC_URL = "jdbc:mysql://localhost:3306/your_database";
     private static final String USERNAME = "root";
     private static final String PASSWORD = "your_password";
 
